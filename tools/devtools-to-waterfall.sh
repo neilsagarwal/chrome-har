995f8aab58cc51dc2cf74af17709ee1ccf242165
Neil Agarwal <neilsagarwal@cs.ucla.edu>
2020-01-02 17:29:02 -0800

    Updated line number

--- tools/devtools-to-waterfall.sh
+++ tools/devtools-to-waterfall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-filepath=<Insert File Path Here>/lighthouse-http-alexa
+filepath=""
 node harCreator.js $filepath/$1.json
 
 file1=index.html
-line=37
+line=39
 
 { head -n $(($line-1)) $file1; cat $1.har; tail -n +$line $file1; } > $1.html
 
